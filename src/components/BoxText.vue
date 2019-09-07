740f70c2ba8933dbdea86c819e0241a2f0563e30
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-07 15:05:56 -0300

    box text component

---
+++ src/components/BoxText.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="border-gray-100 border-4 mb-3 text-sm p-2 w-full">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxText',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
