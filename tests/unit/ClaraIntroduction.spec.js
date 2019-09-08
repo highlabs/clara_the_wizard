4c09f515f5b468b6a108638db2a4936bc5967169
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-08 14:12:27 -0300

    Clara introduction tests

--- tests/unit/ClaraIntroduction.spec.js
+++ tests/unit/ClaraIntroduction.spec.js
@@ -1,20 +1,18 @@
 import { mount } from '@vue/test-utils'
 import ClaraIntroduction from '@/components/ClaraIntroduction.vue'
 describe('ClaraIntroduction.vue', () => {
-  it('continue button exists', () => {
+  it('continue and start buttons steps', () => {
     const wrapper = mount(ClaraIntroduction, {
       propsData: { claraSteps: 0 }
     })
+    const speechLength = wrapper.vm.claraText.length - 1
 
     expect(wrapper.find({ ref: 'continueButton' }).exists()).toBe(true)
-  })
-
-  it('start button exists', () => {
-    const wrapper = mount(ClaraIntroduction)
-    const speechLength = wrapper.vm.claraText.length - 1
+    expect(wrapper.find({ ref: 'startButton' }).exists()).toBe(false)
 
     wrapper.setProps({ claraSteps: speechLength })
 
+    expect(wrapper.find({ ref: 'continueButton' }).exists()).toBe(false)
     expect(wrapper.find({ ref: 'startButton' }).exists()).toBe(true)
   })
 })
