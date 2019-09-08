273ca57f96e6e9d7d235c6d012d27f5712eb0933
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-08 11:09:39 -0300

    Moving all html to components

--- src/App.vue
+++ src/App.vue
@@ -3,59 +3,37 @@
     <section class="p-4 border-gray-100 border-4 relative w-full">
       <Header />
 
-      <div class="flex flex-col w-full items-center h-full justify-between" v-if="claraSteps < 6">
-        <div class="flex items-center h-full">
-          <BoxText :text="claraText[claraSteps]" />
-          <img src="./assets/clara.svg" alt="" class="w-24">
-        </div>
-        <div class="flex flex-col items-end w-full">
-          <button @click="claraSteps = claraSteps + 1" v-if="claraSteps < 5"><span class="text-xs">Continue</span></button>
-          <button @click="start()" v-else><span class="text-xs">Start</span></button>
-        </div>
-      </div>
+      <ClaraIntroduction
+        v-if="claraSteps < 6"
+        :claraSteps="claraSteps"
+        @startClick="start"
+        @incrementSteps="incrementClaraSteps"
+      />
 
       <div v-else class="flex flex-col w-full items-center h-full justify-between">
         <BoxText :text="`Step ${step + 1}`" v-if="step <= 2" />
 
-        <div v-if="step <= 2" class="flex justify-around h-full w-full md:flex-col-reverse">
-          <div class="flex flex-col w-1/4 justify-center items-stretch md:flex-row-reverse md:w-full">
-            <div @click="mini = !mini" class="md:flex md:ml-4 md:h-40 md:justify-center">
-              <Card
-                v-for="pileOne in pile1"
-                :key="pileOne.code"
-                :value="pileOne.value"
-                :mini="mini"
-                :suit="pileOne.suit"
-              />
-            </div>
-            <button class="mt-8 md:mt-1" @click="arrangeCards(pile2, pile1, pile3)">1</button>
-          </div>
+        <div v-if="step <= 2" class="flex justify-around h-full w-full md:flex-col">
+          <PileCards
+            :pile="pile1"
+            @toggleMini="toggleMiniCards"
+            @arrangeCards="arrangeCards(pile2, pile1, pile3)"
+            pileNumber="1"
+          />
 
-          <div class="flex flex-col w-1/4 justify-center items-stretch md:flex-row-reverse md:w-full">
-            <div @click="mini = !mini" class="md:flex md:ml-4 md:h-40 md:justify-center">
-              <Card
-                v-for="pileTwo in pile2"
-                :key="pileTwo.code"
-                :value="pileTwo.value"
-                :mini="mini"
-                :suit="pileTwo.suit"
-              />
-            </div>
-            <button class="mt-8 md:mt-1" @click="arrangeCards(pile1, pile2, pile3)">2</button>
-          </div>
+          <PileCards
+            :pile="pile2"
+            @toggleMini="toggleMiniCards"
+            @arrangeCards="arrangeCards(pile1, pile2, pile3)"
+            pileNumber="2"
+          />
 
-          <div class="flex flex-col w-1/4 justify-center items-stretch md:flex-row-reverse md:w-full">
-            <div @click="mini = !mini" class="md:flex md:ml-4 md:h-40 md:justify-center">
-              <Card
-                v-for="pileThree in pile3"
-                :key="pileThree.code"
-                :value="pileThree.value"
-                :mini="mini"
-                :suit="pileThree.suit"
-              />
-            </div>
-            <button class="mt-8 md:mt-1" @click="arrangeCards(pile1, pile3, pile2)">3</button>
-          </div>
+          <PileCards
+            :pile="pile3"
+            @toggleMini="toggleMiniCards"
+            @arrangeCards="arrangeCards(pile1, pile3, pile2)"
+            pileNumber="3"
+          />
         </div>
 
         <div v-else class="flex flex-col justify-center items-center h-full">
@@ -66,7 +44,7 @@
           <div class="w-32">
             <Card :value="handCards[10].value" :suit="handCards[10].suit" :code="handCards[10].code"/>
           </div>
-          <button @click="again()" class="mt-3"><span class="text-xs">Again</span></button>
+          <Button @onClick="again()" text="again" class="mt-3" />
         </div>
       </div>
 
@@ -79,6 +57,9 @@ import axios from 'axios'
 import Card from './components/Card.vue'
 import BoxText from './components/BoxText'
 import Header from './components/Header'
+import ClaraIntroduction from '@/components/ClaraIntroduction'
+import PileCards from '@/components/PileCards'
+import Button from '@/components/Button'
 
 export default {
   name: 'app',
@@ -92,21 +73,16 @@ export default {
       handCards: [],
       step: 0,
       mini: true,
-      claraSteps: 0,
-      claraText: [
-        "Hi! I'm Clara Oswald! I love magic! Can I show you my favorite trick?",
-        'Cool! I will show you 3 piles of cards...',
-        "And you need to choose one card in just one of the piles. And don't tell me the card!",
-        'When you choose the card, click on the number of the pile',
-        'You will need to point it to me three times for the magic to work!',
-        "Let's start?"
-      ]
+      claraSteps: 0
     }
   },
   components: {
     Card,
     BoxText,
-    Header
+    Header,
+    ClaraIntroduction,
+    PileCards,
+    Button
   },
   methods: {
     async getDeck () {
@@ -163,26 +139,26 @@ export default {
       this.claraSteps = this.claraSteps + 1
       this.dealCards(true)
     },
+    incrementClaraSteps () {
+      this.claraSteps = this.claraSteps + 1
+    },
+    toggleMiniCards () {
+      this.mini = !this.mini
+    },
     again () {
       this.step = 0
       this.dealCards()
     }
   },
-  mounted () {
-    this.$nextTick(function () {
-      this.getDeck()
-    })
+  async mounted () {
+    await this.getDeck()
   }
 }
 </script>
 
-<style scoped>
-button {
-  @apply px-4 py-2 border-4 border-gray-400 mb-1 inline-block whitespace-no-wrap text-sm
-}
-
+<style>
 #app {
-  height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+  height: 100vh;
   height: calc(var(--vh, 1vh) * 100);
 }
 </style>
