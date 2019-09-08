a5cd2a005b3ad410088bdb2bd53b82e9de29521e
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-08 11:09:19 -0300

    Button Component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="px-4 py-2 border-4 border-gray-400 mb-1 inline-block whitespace-no-wrap text-sm" @click="$emit('onClick')">
+    <span class="text-xs">{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
