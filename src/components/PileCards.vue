5f2379ff177d20c2d254b3b2f0709b13bd30288e
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-08 11:09:31 -0300

    Pile components

---
+++ src/components/PileCards.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex flex-col w-1/4 justify-center items-stretch md:flex-row-reverse md:w-full">
+    <div @click="$emit('toggleMini')" class="md:flex md:ml-4 md:h-40 md:justify-center">
+      <Card
+        v-for="card in pile"
+        :key="card.code"
+        :value="card.value"
+        :mini="mini"
+        :suit="card.suit"
+      />
+    </div>
+    <Button class="mt-8 md:mt-1" :text="pileNumber" @onClick="$emit('arrangeCards')" />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card'
+import Button from '@/components/Button'
+
+export default {
+  name: 'PileCards',
+  components: {
+    Card,
+    Button
+  },
+  props: {
+    pile: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    mini: {
+      type: Boolean,
+      default: true
+    },
+    pileNumber: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
