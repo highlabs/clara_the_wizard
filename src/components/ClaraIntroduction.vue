46f99e746286d1035b070b639fc424f8657b9ccf
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-08 11:09:26 -0300

    Introduction components

---
+++ src/components/ClaraIntroduction.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex flex-col w-full items-center h-full justify-between">
+    <div class="flex items-center h-full">
+      <BoxText :text="claraText[claraSteps]" />
+      <img src="../assets/clara.svg" alt="" class="w-24">
+    </div>
+    <div class="flex flex-col items-end w-full">
+      <Button @onClick="$emit('incrementSteps')" text="Continue" v-if="claraSteps < 5"/>
+      <div v-else>
+        <Button @onClick="$emit('startClick')" text="Start" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BoxText from '@/components/BoxText'
+import Button from '@/components/Button'
+
+export default {
+  name: 'ClaraIntroduction',
+  components: {
+    BoxText,
+    Button
+  },
+  data () {
+    return {
+      claraText: [
+        "Hi! I'm Clara Oswald! I love magic! Can I show you my favorite trick?",
+        'Cool! I will show you 3 piles of cards...',
+        "And you need to choose one card in just one of the piles. And don't tell me the card!",
+        'When you choose the card, click on the number of the pile',
+        'You will need to point it to me three times for the magic to work!',
+        "Let's start?"
+      ]
+    }
+  },
+  props: {
+    claraSteps: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
